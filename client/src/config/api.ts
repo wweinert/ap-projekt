302de9c49e34825f101576bd41cf1a81a1296a12
woldemst <wald.weinert@gmail.com>
2026-05-19 20:55:09 +0200

    ip condig

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -1,7 +1,8 @@
 import { Platform } from "react-native";
 
 // DEV
-const HOST = Platform.OS === "web" ? "localhost" : "192.168.10.91"; //  PC || android : ios
+// const HOST = Platform.OS === "web" ? "localhost" : "192.168.10.91"; //  PC : android || ios | WORK
+const HOST = Platform.OS === "web" ? "localhost" : "192.168.178.44";  //  PC : android || ios | HOME
 
 // const HOST = "91.99.224.106" // itnt server
 // const HOST2 = "" // pi server
